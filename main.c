ddae191294b1f8882adec8b66e1c7dd3dcd5d6c8
Ricardo De Anda <jose.dean@uanl.edu.mx>
2019-08-24 10:22:09 -0500

    Commit para Practica3

--- main.c
+++ main.c
@@ -4,5 +4,6 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
+	//Este comentario es para identificar el branch Practica3
 	return 0;
 }
