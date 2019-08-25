ca3fd504f5468398587d491e6d3f3432284327c0
Ricardo De Anda <jose.dean@uanl.edu.mx>
2019-08-25 16:21:00 -0500

    Problema 2

--- main.c
+++ main.c
@@ -21,6 +21,10 @@ int main() {
 	output = (x-1)/4.0 - (x-5)/36.0;
 	printf("El output de la primera funcion algebraica es: %f \n", output);
 
+	// Calculo para la segunda funcion algebraica
+	output = 6*((x+1)/8.0 - (2*x-3)/16.0);
+	printf("El output de la segunda funcion algebraica es: %f \n", output);
+
 
 
 	return 0;
