e8a272d9973dd5a6358b2d5d2f6a0462ed75d060
Ricardo <jose.dean@uanl.edu.mx>
2019-09-28 09:56:36 -0500

    Practica8

--- main.c
+++ main.c
@@ -1,8 +1,44 @@
+/*
+* Autor: Jose Ricardo De Anda Caballero
+* Fecha: 28/09/2019
+* Grupo: 008
+* Horario: Sabado 9am-11am
+* Matricula: 1447001
+*/
+
 #include <stdio.h>
-#include <stdlib.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int main() {
 
-int main(int argc, char *argv[]) {
+	//**************ACTIVIDAD1*****************
+
+	// Declaracion de variables
+	int numeroDeCoches[24], R = 2, i;
+
+	// Llenamos el arreglo con valores
+	for(i=0; i<24; i++) 
+		numeroDeCoches[i] = i;
+
+	// Modificamos el valor del arreglo en la posicion R
+	numeroDeCoches[R] = 57;
+
+	printf("El valor del arreglo en la posicion %d es: %d\n", R, numeroDeCoches[R]);
+
+	R = 21;
+
+	// Modificamos el valor del arreglo en la posicion R
+	numeroDeCoches[R] = 57;
+
+	printf("El valor del arreglo en la posicion %d es: %d\n", R, numeroDeCoches[R]);
+	
+
+	//**************ACTIVIDAD2*****************
+	
+	int A = 8, B = 4;
+	// Calculo para el nuevo index
+	R = A/B;
+
+	printf("El valor del arreglo en la posicion A/B = %d es: %d\n", R, numeroDeCoches[R]);
+
 	return 0;
 }
