7674ef35d7561310816239faa18e9d8c472b3f73
ricardouanl <jose.dean@uanl.edu.mx>
2019-10-07 21:02:48 -0500

    Practica9

--- main.c
+++ main.c
@@ -1,8 +1,34 @@
+/*
+* Autor: Jose Ricardo De Anda Caballero
+* Fecha: 07/10/2019
+* Grupo: 008
+* Horario: Sabado 9am-11am
+* Matricula: 1447001
+*/
+
 #include <stdio.h>
-#include <stdlib.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int main() {
 
-int main(int argc, char *argv[]) {
+	//**************ACTIVIDAD1*****************
+
+	// Declaracion del arreglo
+	int ventasEmpleadoOficinaEdificio[6][10][2];
+
+	// Asignacion del valor
+	ventasEmpleadoOficinaEdificio[3][7][2] = 4234;
+
+	printf("El empleado numero 3 de la oficina numero 7 del edificio numero 2 ha vendido %d unidades \n", ventasEmpleadoOficinaEdificio[3][7][2]);
+
+	// a) 120.
+	// b) Se accede a otra direccion de memoria que nos regresa un valor incorrecto.
+
+	//**************ACTIVIDAD2*****************
+
+	// Agregamos 10 unidades al valor anterior
+	ventasEmpleadoOficinaEdificio[3][7][2] = ventasEmpleadoOficinaEdificio[3][7][2] + 10;
+
+	printf("El empleado numero 3 de la oficina numero 7 del edificio numero 2 ha vendido %d unidades", ventasEmpleadoOficinaEdificio[3][7][2]);
+
 	return 0;
 }
