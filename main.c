c83e7f6d8d06585ea994ac4c205c79e02e95765c
Ricardo <jose.dean@uanl.edu.mx>
2019-10-12 10:35:18 -0500

    Actividad1

--- main.c
+++ main.c
@@ -1,8 +1,60 @@
+/*
+* Autor: Jose Ricardo De Anda Caballero
+* Fecha: 12/10/2019
+* Grupo: 008
+* Horario: Sabado 9am-11am
+* Matricula: 1447001
+*/
+
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int main() {
 
-int main(int argc, char *argv[]) {
+	//**************ACTIVIDAD1*****************
+
+	char str[100];
+
+	char str2[100];
+	int str2Len = 0;
+
+	int i = 0;
+	int palindromo = 1;
+
+	printf("Ingrese la cadena \n");
+	gets(str);
+
+	for (i=0; i<strlen(str); i++) {
+		if ( str[i] != ' ' ) {
+			str2[str2Len] = str[i];
+			str2Len++;
+		}
+	}
+
+
+	for (i = 0; i < strlen(str2); i++) {
+
+		if( str2[i] != str2[strlen(str2)-i-1] )
+			palindromo = 0;
+	}
+	
+	if (palindromo == 1)
+		printf("La cadena es palindromo");
+	else
+		printf("La cadena no es palindromo");
+
+
+	// Declaracion del arreglo
+	
+	
+
+	// Asignacion del valor
+
+
+	//**************ACTIVIDAD2*****************
+
+
+
 	return 0;
 }
+
